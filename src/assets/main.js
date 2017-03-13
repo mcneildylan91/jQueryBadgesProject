5687bb858d13a285b05842b2800fd7b12d5cf841
mcneildylan91 <dmcneil@hawaii.edu>
2017-03-12 18:42:26 -1000

    test 1
    test

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,38 @@
 $(function() {
 
-  // your code will go here
+$.ajax({
+  url: 'https://www.codeschool.com/users/mcneildylan91.json',
+  dataType: 'jsonp',
+  success: function(response){
+
+    addCourses(response.courses.completed);
+  }
+});
+
+function addCourses(courses){
+
+var $badges = $('#badges');
+
+  courses.forEach(function(course) {
+    $('<div />', {
+      'class': 'courses'
+    }).appendTo($badges);
+
+    $('<h3 />', {
+      text: course.title
+    }).appendTo($course);
+
+    $('<img />', {
+      src: course.badge
+    }).appendTo($course);
+
+    $('<a />',{
+      'class': 'btn btn-primary',
+      target: '_blank',
+      href: course.url,
+      text: 'See course'
+    }).appendTo($course);
+  })
+}
 
 });
